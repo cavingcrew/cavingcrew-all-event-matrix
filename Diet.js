9cc80c8fb5faae581ae290b338c3a01806481da0
Tim Dobson <hello@tdobson.net>
2024-09-17 11:57:22 +0100

    fix: Update Diet.js to remove unnecessary columns and format dietary requirements

--- Diet.js
+++ Diet.js
@@ -1,11 +1,11 @@
 function readDiet(stmt) {
   let cell = setupCell("Dashboard", "B49");
   let sheet = setupSheet("Diet");
- 
-  var results = stmt.executeQuery('select distinct `first_name` as "First Name", `nickname` as "Facebook Name", `admin-dietary-requirements` as "Dietary Requirements", `admin-diet-allergies-health-extra-info` "Diet and Health Details", cc_attendance as "Attendance", status as "Status", pd.order_id as "Order ID", user_id as "User ID" from jtl_member_db db LEFT JOIN jtl_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND pd.status="wc-processing" order by `admin-dietary-requirements` ASC');
+
+  var results = stmt.executeQuery('select distinct `first_name` as "First Name", `nickname` as "Facebook Name", `admin-dietary-requirements` as "Dietary Requirements", `admin-diet-allergies-health-extra-info` "Diet and Health Details", pd.order_id as "Order ID" from jtl_member_db db LEFT JOIN jtl_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND pd.status="wc-processing" order by `admin-dietary-requirements` ASC');
 
   appendToSheet(sheet, results);
-  
+
   setColoursFormat(sheet, "C2:C", "Vegan", "#e6ffe6");
   setColoursFormat(sheet, "C2:C", "Vegetarian", "#e6ffe6");
   setTextFormat(sheet, "D2:D", "No", "#a9a9a9");
