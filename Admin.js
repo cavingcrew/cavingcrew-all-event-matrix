eaee9e3726fd3a1a6b6bc279645ebe5c7eee95a0
Tim Dobson <hello@tdobson.net>
2024-09-17 11:28:17 +0100

    feat: Add consistent "Order ID" column label across reports

--- Admin.js
+++ Admin.js
@@ -2,7 +2,7 @@ function readAdmin(stmt) {
   let cell = setupCell("Dashboard", "B49");
   let sheet = setupSheet("Admin");
  
-  var results = stmt.executeQuery('select distinct `first_name` "First Name", `last_name` "Last Name", `nickname` "Facebook Name", `admin-phone-number` "Phone Number", `admin-participation-statement-one` "Participation Statement 1", `admin-participation-statement-two` "Participation Statement 2", `admin-car-registration` "Car Registration", `admin-emergency-contact-name` "Emergency Contact Name", `admin-emergency-contact-phone` "Emergency Contact Phone" from jtl_member_db db LEFT JOIN jtl_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `first_name` ASC');
+  var results = stmt.executeQuery('select distinct `first_name` "First Name", `last_name` "Last Name", `nickname` "Facebook Name", `admin-phone-number` "Phone Number", `admin-participation-statement-one` "Participation Statement 1", `admin-participation-statement-two` "Participation Statement 2", `admin-car-registration` "Car Registration", `admin-emergency-contact-name` "Emergency Contact Name", `admin-emergency-contact-phone` "Emergency Contact Phone", pd.order_id "Order ID" from jtl_member_db db LEFT JOIN jtl_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `first_name` ASC');
 
   appendToSheet(sheet, results);
   
--- Transport.js
+++ Transport.js
@@ -2,7 +2,7 @@ function readTransport(stmt) {
   let cell = setupCell("Dashboard", "B49");
   let sheet = setupSheet("Transport");
  
-  var results = stmt.executeQuery('select `first_name` "First Name", `nickname` "Facebook Name", `transport-need-lift` "Needs Lift", `transport-will-you-give-lift` "Offering Lift", `transport-leaving-location` "Leaving From", `transport-depature-time` "Departure Time" from jtl_member_db db LEFT JOIN jtl_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
+  var results = stmt.executeQuery('select `first_name` "First Name", `nickname` "Facebook Name", `transport-need-lift` "Needs Lift", `transport-will-you-give-lift` "Offering Lift", `transport-leaving-location` "Leaving From", `transport-depature-time` "Departure Time", pd.order_id "Order ID" from jtl_member_db db LEFT JOIN jtl_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
 
   appendToSheet(sheet, results);
   
