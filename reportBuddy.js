3815416dc8804f1a5550f8a96a782b624d23ff64
Tim Dobson <hello@tdobson.net>
2024-09-23 17:12:20 +0100

    fix: Update SQL query in reportBuddy.js

--- reportBuddy.js
+++ reportBuddy.js
@@ -6,12 +6,11 @@ function readBuddy(stmt, cell) {
         \`first_name\` AS "First Name",
         \`nickname\` AS "Facebook Name",
         \`admin-first-timer-question\` AS "First Timer",
-        \`_order_count\` AS "Order Count",
         pd.order_id AS "Order ID"
       FROM jtl_member_db db
       LEFT JOIN jtl_order_product_customer_lookup pd ON pd.user_id = db.id
       WHERE product_id=${cell} AND status="wc-processing"
-      ORDER BY \`_order_count\` ASC
+      ORDER BY \`first_name\` ASC
     `,
     formatting: [
       { type: 'color', column: "First Timer", search: "Yes", color: colors.lightRed }
